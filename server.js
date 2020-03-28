8a2a58f0dede897e3d626333745f180541651163
Sam Delay <samuel.delay@gmail.com>
2020-03-28 19:45:31 -0400

    server structure

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+const db = require("./models");
+
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,11 +16,13 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/controller.js");
+const routes = require("./routes/burger-api.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
 
