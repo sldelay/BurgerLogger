0d6dac17dc0658ba3b8cbcf64ed9da3d85882421
Sam Delay <samuel.delay@gmail.com>
2020-03-28 19:45:43 -0400

    Burger model

---
+++ models/burger.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("burger", {
+      burger_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      devoured: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    });
+    return Burger;
+  };
