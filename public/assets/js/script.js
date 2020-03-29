391ffbf99f9b8d695e2848f769efc8db67da7a25
Sam Delay <samuel.delay@gmail.com>
2020-03-29 17:42:36 -0400

    event listeners

---
+++ public/assets/js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+    $(".newBurger").on("submit", function(event) {
+      event.preventDefault();
+
+      let newBurger = {
+        burger: $("#burgerName").val().trim()
+      }
+
+      $.post("/api/burger", newBurger, function() {
+        window.location.href = "/";
+      });
+    })
+
+    $(document).on("click", "#devour", function() {
+        let id = $(this).attr('data-id');
+        console.log(id)
+
+        let justEaten = {
+          devoured: true
+        }
+
+        $.ajax({
+          method: "PUT",
+          url: `/api/burger/${id}`,
+          data: justEaten
+        })
+          .then(function() {
+            window.location.href = "/";
+          });
+
+    })
+});
+
+
