3f6e0f097ce51761924d467af4baffddd460dda3
Sam Delay <samuel.delay@gmail.com>
2020-03-29 17:42:51 -0400

    finished routes

--- routes/burger-api.js
+++ routes/burger-api.js
@@ -4,15 +4,37 @@ const db = require("../models");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    db.Burger.findAll({}).then(function(dbBurger) {
- 
-        res.render("index", {
-            id: dbBurger.id,
-            burger: dbBurger.burger_name,
-            devoured: dbBurger.devoured
-        });
-      });
-  });
+router.get("/", function (req, res) {
+    db.Burger.findAll({
+        attributes: ["id", "burger_name", "devoured"],
+        raw: true,
+        order: [['updatedAt', 'DESC']]
+    }).then(function (data) {
+        res.render('index', {
+            data
+        })
+    });
+});
 
-  module.exports = router;
+router.post("/api/burger", function (req, res) {
+    db.Burger.create({
+        burger_name: req.body.burger
+    }).then(function (data) {
+        res.json(data)
+    });
+});
+
+router.put("/api/burger/:id", function (req, res) {
+    db.Burger.update(req.body,
+        {
+          where: {
+            id: req.params.id
+          }
+        }).then(function(data) {
+        res.json(data);
+      });
+})
+
+
+
+module.exports = router;
